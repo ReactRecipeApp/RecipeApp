cde5267829c64e426ba209b396244465e1807e4a
nik0llaa <nikolastojkovic255@gmail.com>
2024-05-23 22:55:31 +0200

    izmena

--- src/indexCreateReceipt.tsx
+++ src/indexCreateReceipt.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppBar from './appBar';
+import App from './App';
 import Grid1 from './Grid';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,8 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <AppBar />
-    <Grid1 />
+    <App />
   </React.StrictMode>
 );
 
